30c9ad22695a7f02c752e5202143e2c2d01b5625
toastal <toastal@posteo.net>
2024-06-14 16:30:58 +0700

    internal: refactor html rendering

--- src/render.rs
+++ src/render.rs
@@ -35,9 +35,9 @@ impl<'a> Admonition<'a> {
         }
     }
 
-    pub(crate) fn html(&self, id_counter: &mut HashMap<String, usize>) -> String {
+    pub(crate) fn html(self, id_counter: &mut HashMap<String, usize>) -> String {
         let anchor_id = match &self.css_id {
-            CssId::Verbatim(id) => Cow::Borrowed(id),
+            CssId::Verbatim(id) => Cow::Borrowed(id.as_str()),
             CssId::Prefix(prefix) => {
                 let id = unique_id_from_content(
                     if !self.title.is_empty() {
@@ -56,53 +56,68 @@ impl<'a> Admonition<'a> {
         let content = &self.content;
         let indent = " ".repeat(self.indent);
 
-        let title_block = if self.collapsible { "summary" } else { "div" };
-
-        let title_html = if !title.is_empty() {
-            Cow::Owned(format!(
-                r##"{indent}<{title_block} class="admonition-title">
+        let (titlebar_html, _title_id) = if !title.is_empty() {
+            let titlebar_element = if self.collapsible { "summary" } else { "div" };
+            let title_id = format!("{anchor_id}-title");
+            let titlebar_html = Cow::Owned(format!(
+                r##"{indent}<{titlebar_element} class="admonition-title">
 {indent}
 {indent}{title}
 {indent}
 {indent}<a class="admonition-anchor-link" href="#{anchor_id}"></a>
-{indent}</{title_block}>
+{indent}</{titlebar_element}>
 "##
-            ))
+            ));
+            (titlebar_html, Some(title_id))
         } else {
-            Cow::Borrowed("")
+            (Cow::Borrowed(""), None)
         };
 
-        let mut additional_class = format!("admonish-{}", self.directive);
-        if !self.additional_classnames.is_empty() {
-            for additional_classname in &self.additional_classnames {
-                additional_class.push(' ');
-                additional_class.push_str(additional_classname);
-            }
-        }
+        let mut classes = vec![
+            "admonition".to_owned(),
+            format!("admonish-{}", self.directive),
+        ];
+        classes.extend(self.additional_classnames);
+        let classes = classes.join(" ");
 
-        let admonition_block = if self.collapsible { "details" } else { "div" };
+        let mut attributes = vec![("id", anchor_id), ("class", Cow::Owned(classes))];
+        let attributes = join_attributes(&attributes);
+
+        let admonition_element = if self.collapsible { "details" } else { "div" };
         // Notes on the HTML template:
         // - the additional whitespace around the content are deliberate
         //   In line with the commonmark spec, this allows the inner content to be
         //   rendered as markdown paragraphs.
         format!(
             r#"
-{indent}<{admonition_block} id="{anchor_id}" class="admonition {additional_class}">
-{title_html}{indent}<div>
+{indent}<{admonition_element} {attributes}>
+{titlebar_html}{indent}<div>
 {indent}
 {indent}{content}
 {indent}
 {indent}</div>
-{indent}</{admonition_block}>"#,
+{indent}</{admonition_element}>"#,
         )
     }
 
     /// Strips all admonish syntax, leaving the plain content of the block.
-    pub(crate) fn strip(&self) -> String {
+    pub(crate) fn strip(self) -> String {
         // Add in newlines to preserve line numbering for test output
         // These replace the code fences we stripped out
         format!("\n{}\n", self.content)
     }
+}
+
+fn join_attributes(attributes: &[(impl AsRef<str>, impl AsRef<str>)]) -> String {
+    let mut buffer = String::new();
+    for (key, value) in attributes {
+        buffer.push_str(key.as_ref());
+        buffer.push_str(r#"=""#);
+        buffer.push_str(value.as_ref());
+        buffer.push_str(r#"" "#);
+    }
+    buffer.pop();
+    buffer
 }
 
 const ANCHOR_ID_DEFAULT: &str = "default";
